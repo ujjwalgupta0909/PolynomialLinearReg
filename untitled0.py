ffd9d1b35510268083daa4cb9473265ffb75d052
ujjwalgupta0909 <44123783+ujjwalgupta0909@users.noreply.github.com>
2019-03-10 15:48:13 +0530

    Add files via upload

---
+++ untitled0.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 29 17:32:42 2018
+
+@author: hp
+"""
+
+#polynormial regression
+#x is feature and t islabel
+#t=x^n+x^n-1+...
+
+#underfitting model--traindata and test data both have low score
+#overfitten-train is very good but test is poor
+
+#SUPERVISED ML->CLASSIFICATION->LOGISTIC REGRESSION.
+#logistic regression gives probability ..IF ITS IS >.5 THEN YES..ELSE NO
